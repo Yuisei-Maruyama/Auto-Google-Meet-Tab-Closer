6939cca7c8bcbd764de5e65ff6ba811a3d42f203
Yuisei-Maruyama <yuisei.metro@gmail.com>
2023-05-03 02:25:12 +0900

    fix: 対象者が「通話から退出」のボタンを押下したことを購読して、タブが閉じるように修正

--- background.ts
+++ background.ts
@@ -1,70 +1,34 @@
-interface AddedNode {
-  textContent: string
-}
-
 interface Message {
   closeTab: boolean
 }
 
-function checkTargetText(addedNode: Node, targetText: string): boolean {
-  const textContent = addedNode.textContent || ''
-  return textContent.includes(targetText)
-}
-
-function processMutation(
-  mutation: MutationRecord,
-  targetText: string
-): boolean {
-  return Array.from(mutation.addedNodes).some((addedNode: Node) => {
-    const isTargetTextFound = checkTargetText(addedNode, targetText)
-    if (isTargetTextFound) {
-      chrome.runtime.sendMessage({ closeTab: true })
-    }
-    return isTargetTextFound
-  })
-}
-
-function processMutations(
-  mutations: MutationRecord[],
-  targetText: string,
-  observer: MutationObserver
-): void {
-  const found = mutations.some((mutation) =>
-    processMutation(mutation, targetText)
+function addEndCallListener() {
+  const endCallButton = document.querySelector(
+    'button[aria-label="通話から退出"]'
   )
-  if (found) {
-    observer.disconnect()
+
+  if (endCallButton) {
+    endCallButton.addEventListener('click', () => {
+      chrome.runtime.sendMessage({ closeTab: true })
+    })
+  } else {
+    setTimeout(addEndCallListener, 500)
   }
 }
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(
   (details: chrome.webNavigation.WebNavigationTransitionCallbackDetails) => {
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId },
-      func: function () {
-        const targetText = 'ミーティングから退出しました'
-
-        const observer = new MutationObserver((mutations) => {
-          for (const mutation of mutations) {
-            for (const addedNode of mutation.addedNodes) {
-              if (
-                addedNode.textContent &&
-                addedNode.textContent.includes(targetText)
-              ) {
-                chrome.runtime.sendMessage({ closeTab: true })
-                observer.disconnect()
-                return
-              }
-            }
-          }
-        })
-
-        observer.observe(document.body, {
-          childList: true,
-          subtree: true,
-        })
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: details.tabId },
+        func: addEndCallListener,
       },
-    })
+      () => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError.message)
+        }
+      }
+    )
   },
   {
     url: [{ urlMatches: 'https://meet.google.com/*' }],
